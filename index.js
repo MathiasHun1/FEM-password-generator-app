e482ee2f213595ea5a17a4443375c802b76067d3
snus4me <mathias.hun@gmail.com>
2025-01-30 20:16:05 +0100

    UI 1.0 done, next implement functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+// Maunipulate the slider
+const sliderInput = document.getElementById('slider-input');
+const sliderOutput = document.getElementById('slider-output');
+
+sliderOutput.value = sliderInput.value;
+
+sliderInput.addEventListener('input', (e) => {
+  sliderOutput.value = e.target.value;
+
+  const min = sliderInput.min;
+  const max = sliderInput.max;
+  const currentValue = sliderInput.value;
+
+  sliderInput.style.backgroundSize = `${
+    ((currentValue - min) / (max - min)) * 100
+  }% 100%`;
+});
+
+// password strenght criterions:
+// TOO WEAK: <= 4
+// WEAK: ( 5 <= pass < 8 AND 2type atl ) OR ( >= 8 AND 1type )
+// MEDIUM: (8 <= pass < 12 AND 3type atl) OR ( >= 12 AND 2type )
+// STRONG: (8 <= pass < 12 AND 4type) OR ( >= 12 AND 3type)
